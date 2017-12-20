e191ac6bea1f33799673840e8ff368a747b95cac
Sunny Huang <sizzlorox@gmail.com>
2017-12-20 16:29:39 -0200

    removed test mock map

--- idle-rpg/game/Game.js
+++ idle-rpg/game/Game.js
@@ -67,16 +67,6 @@ class Game {
 
   attackEvent(selectedPlayer, onlinePlayers, twitchBot) {
     const luckDice = helper.randomBetween(0, 100);
-    selectedPlayer.map = {
-      id: 10,
-      name: 'Wintermere',
-      type: {
-        id: 1,
-        name: 'Snow'
-      },
-      levelReq: 1
-    };
-
     if (Event.MapClass.getTowns().includes(selectedPlayer.map.name) && luckDice <= 30 + (selectedPlayer.stats.luk / 2)) {
       return Event.generateTownItemEvent(this.discordHook, twitchBot, selectedPlayer);
     }
