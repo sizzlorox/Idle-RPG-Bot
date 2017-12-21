1ab60ddbbc88f9895c79b556e727e647a3b52075
Sunny Huang <sizzlorox@gmail.com>
2017-12-20 22:39:26 -0200

    previous owner name not showing, luck event missing case, balanced player death

--- idle-rpg/game/Game.js
+++ idle-rpg/game/Game.js
@@ -82,6 +82,7 @@ class Game {
 
   luckEvent(selectedPlayer, twitchBot) {
     const luckDice = helper.randomBetween(0, 100);
+    console.log(`Player: ${selectedPlayer.name} - Dice: ${luckDice}`);
     if (luckDice <= 5 + (selectedPlayer.stats.luk / 2)) {
       return Event.generateGodsEvent(this.discordHook, twitchBot, selectedPlayer);
     } else if (getTowns().includes(selectedPlayer.map.name) && luckDice <= 20 + (selectedPlayer.stats.luk / 2)) {
