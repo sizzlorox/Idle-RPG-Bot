91b9324e49b0908f0db93d0d41baa4f33aa81f9c
Sunny Huang <sizzlorox@gmail.com>
2018-01-31 03:37:39 -0200

    fix bounty bug

--- idle-rpg/game/utils/Event.js
+++ idle-rpg/game/utils/Event.js
@@ -335,7 +335,7 @@ class Event {
   stealPlayerItem(discordHook, twitchBot, selectedPlayer, randomPlayer) {
     return new Promise((resolve) => {
       const luckStealChance = helper.randomBetween(0, 100);
-      const chance = Math.floor((selectedPlayer.currentBounty * Math.log(1.2)) / 100);
+      const chance = Math.floor((randomPlayer.currentBounty * Math.log(1.2)) / 100);
 
       if (luckStealChance > 90 - !Number.isFinite(chance) ? 0 : chance) {
         const luckItem = helper.randomBetween(0, 2);
