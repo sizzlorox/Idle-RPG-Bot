a07ba17e264da3ed046965a42c7f49d8dae49788
Sunny Huang <sizzlorox@gmail.com>
2018-02-21 16:18:37 -0300

    possible fix for incorrect stolen item name

--- idle-rpg/game/data/events.js
+++ idle-rpg/game/data/events.js
@@ -136,25 +136,26 @@ const events = {
       stealingPlayer.stole++;
       if (victimPlayer.equipment[itemKey].name !== enumHelper.equipment.empty[itemKey].name) {
         stealingPlayer.equipment[itemKey].position = itemKey;
+        victimPlayer.equipment[itemKey].position = itemKey;
         if (Helper.calculateItemRating(stealingPlayer, stealingPlayer.equipment[itemKey]) < Helper.calculateItemRating(victimPlayer, victimPlayer.equipment[itemKey])) {
           stealingPlayer = Helper.setPlayerEquipment(stealingPlayer, enumHelper.equipment.types[itemKey].position, stolenEquip);
-          if (!victimPlayer.equipment[itemKey].previousOwners) {
-            stealingPlayer.equipment[itemKey].previousOwners = [`${victimPlayer.name}`];
-          } else {
+          if (victimPlayer.equipment[itemKey].previousOwners.length > 0) {
             stealingPlayer.equipment[itemKey].previousOwners = victimPlayer.equipment[itemKey].previousOwners;
             stealingPlayer.equipment[itemKey].previousOwners.push(victimPlayer.name);
-          }
-          if (victimPlayer.inventory.equipment.length > 0 && victimPlayer.inventory.equipment.find('position', enumHelper.equipment.types[itemKey].position) !== undefined) {
-            const equipFromInventory = victimPlayer.inventory.equipment.filter(equipment => equipment.position === enumHelper.equipment.types[itemKey].position)
-              .sort((item1, item2) => {
-                return item1.power - item2.power;
-              })[0];
-            victimPlayer = Helper.setPlayerEquipment(victimPlayer, enumHelper.equipment.types[itemKey].position, equipFromInventory);
           } else {
-            victimPlayer = Helper.setPlayerEquipment(victimPlayer, enumHelper.equipment.types[itemKey].position, enumHelper.equipment.empty[itemKey]);
+            stealingPlayer.equipment[itemKey].previousOwners = [`${victimPlayer.name}`];
           }
         } else {
           stealingPlayer = InventoryManager.addEquipmentIntoInventory(stealingPlayer, stolenEquip);
+        }
+        if (victimPlayer.inventory.equipment.length > 0 && victimPlayer.inventory.equipment.find('position', enumHelper.equipment.types[itemKey].position) !== undefined) {
+          const equipFromInventory = victimPlayer.inventory.equipment.filter(equipment => equipment.position === enumHelper.equipment.types[itemKey].position)
+            .sort((item1, item2) => {
+              return item1.power - item2.power;
+            })[0];
+          victimPlayer = Helper.setPlayerEquipment(victimPlayer, enumHelper.equipment.types[itemKey].position, equipFromInventory);
+        } else {
+          victimPlayer = Helper.setPlayerEquipment(victimPlayer, enumHelper.equipment.types[itemKey].position, enumHelper.equipment.empty[itemKey]);
         }
       }
     }
