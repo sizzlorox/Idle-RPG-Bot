5077f362b835f6360e87a3f6a7ee12a17147fda6
Sunny Huang <sizzlorox@gmail.com>
2018-03-26 10:51:03 -0300

    refactor to database class promises

--- idle-rpg/database/Database.js
+++ idle-rpg/database/Database.js
@@ -51,63 +51,57 @@ class Database {
 
   createNewPlayer(discordId, name) {
     connect();
-    return new Promise((resolve, reject) => {
-      return Player.create(newPlayerObj(discordId, name), (err, result) => {
-        if (err) {
-          disconnect();
-          return reject(err);
-        }
-        // console.log(`DATABASE: ${discordId} has been created.`);
+    return new Promise((resolve, reject) => Player.create(newPlayerObj(discordId, name), (err, result) => {
+      if (err) {
         disconnect();
-        return resolve(result);
-      });
-    });
+        return reject(err);
+      }
+      // console.log(`DATABASE: ${discordId} has been created.`);
+      disconnect();
+      return resolve(result);
+    }));
   }
 
   loadOnlinePlayers(discordId) {
     connect();
-    return new Promise((resolve, reject) => {
-      return Player.find({}, (err, result) => {
-        if (err) {
-          disconnect();
-          return reject(err);
-        }
-
+    return new Promise((resolve, reject) => Player.find({}, (err, result) => {
+      if (err) {
         disconnect();
-        return resolve(result);
-      })
-        .where('discordId')
-        .select({
+        return reject(err);
+      }
 
-        })
-        .in(discordId);
-    });
+      disconnect();
+      return resolve(result);
+    })
+      .where('discordId')
+      .select({
+
+      })
+      .in(discordId));
   }
 
   loadOnlinePlayerMaps(discordIds) {
     connect();
     const removeNpcs = enumHelper.mockPlayers.map(npc => npc.name);
 
-    return new Promise((resolve, reject) => {
-      return Player.find({
-        name: { $nin: removeNpcs, $exists: true }
-      }, (err, result) => {
-        if (err) {
-          disconnect();
-          return reject(err);
-        }
-        // console.log('DATABASE: Multiple IDs has been loaded from the Database.');
+    return new Promise((resolve, reject) => Player.find({
+      name: { $nin: removeNpcs, $exists: true }
+    }, (err, result) => {
+      if (err) {
         disconnect();
-        return resolve(result);
+        return reject(err);
+      }
+      // console.log('DATABASE: Multiple IDs has been loaded from the Database.');
+      disconnect();
+      return resolve(result);
+    })
+      .where('discordId')
+      .select({
+        discordId: 1,
+        name: 1,
+        map: 1
       })
-        .where('discordId')
-        .select({
-          discordId: 1,
-          name: 1,
-          map: 1
-        })
-        .in(discordIds);
-    });
+      .in(discordIds));
   }
 
   loadTop10(type) {
@@ -128,36 +122,32 @@ class Database {
       name: { $nin: removeNpcs, $exists: true }
     };
 
-    return new Promise((resolve, reject) => {
-      return Player.find(query, (err, result) => {
-        if (err) {
-          disconnect();
-          return reject(err);
-        }
-
+    return new Promise((resolve, reject) => Player.find(query, (err, result) => {
+      if (err) {
         disconnect();
-        return resolve(result);
-      })
-        .select(select)
-        .sort(type)
-        .limit(10);
-    });
+        return reject(err);
+      }
+
+      disconnect();
+      return resolve(result);
+    })
+      .select(select)
+      .sort(type)
+      .limit(10));
   }
 
   loadPlayer(discordId, selectFields = {}) {
     connect();
-    return new Promise((resolve, reject) => {
-      return Player.findOne({ discordId }, (err, result) => {
-        if (err) {
-          disconnect();
-          return reject(err);
-        }
-        // console.log(`DATABASE: ${discordId} has been loaded from the Database.`);
+    return new Promise((resolve, reject) => Player.findOne({ discordId }, (err, result) => {
+      if (err) {
         disconnect();
-        return resolve(result);
-      })
-        .select(selectFields);
-    });
+        return reject(err);
+      }
+      // console.log(`DATABASE: ${discordId} has been loaded from the Database.`);
+      disconnect();
+      return resolve(result);
+    })
+      .select(selectFields));
   }
 
   savePlayer(player) {
@@ -166,17 +156,15 @@ class Database {
     }
 
     connect();
-    return new Promise((resolve, reject) => {
-      return Player.findOneAndUpdate({ discordId: player.discordId }, player, (err, result) => {
-        if (err) {
-          disconnect();
-          return reject(err);
-        }
-        // console.log(`DATABASE: ${player.discordId} has been saved into the Database.`);
+    return new Promise((resolve, reject) => Player.findOneAndUpdate({ discordId: player.discordId }, player, (err, result) => {
+      if (err) {
         disconnect();
-        return resolve(result);
-      });
-    });
+        return reject(err);
+      }
+      // console.log(`DATABASE: ${player.discordId} has been saved into the Database.`);
+      disconnect();
+      return resolve(result);
+    }));
   }
 
   getSameMapPlayers(playerMap) {
@@ -185,134 +173,126 @@ class Database {
     }
 
     connect();
-    return new Promise((resolve, reject) => {
-      return Player.find({ 'map.name': playerMap }, (err, result) => {
-        if (err) {
-          disconnect();
-          return reject(err);
-        }
+    return new Promise((resolve, reject) => Player.find({ 'map.name': playerMap }, (err, result) => {
+      if (err) {
         disconnect();
-        return resolve(result);
-      });
-    });
+        return reject(err);
+      }
+      disconnect();
+      return resolve(result);
+    }));
   }
 
   deletePlayer(playerId) {
     connect();
-    return new Promise((resolve, reject) => {
-      return Player.remove({ discordId: playerId }, (err, result) => {
-        if (err) {
-          disconnect();
-          return reject(err);
-        }
+    return new Promise((resolve, reject) => Player.remove({ discordId: playerId }, (err, result) => {
+      if (err) {
         disconnect();
-        return resolve(result);
-      })
-    });
+        return reject(err);
+      }
+      disconnect();
+      return resolve(result);
+    }));
   }
 
   resetAllPlayers() {
     connect();
-    return new Promise((resolve, reject) => {
-      return Player.update({},
-        {
-          $set: {
-            class: 'Wanderer',
-            health: 105,
-            mana: 50,
-            experience: {
-              current: 0,
-              total: 0
-            },
-            map: MapClass.getRandomTown(),
-            level: 1,
-            gold: {
-              current: 0,
-              stolen: 0,
-              stole: 0,
-              total: 0
-            },
-            'equipment.helmet': {
-              name: 'Nothing',
-              power: 0.15,
-              previousOwners: []
-            },
-            'equipment.armor': {
-              name: 'Linen Shirt',
-              power: 0.75,
-              position: 'armor',
-              previousOwners: []
-            },
-            'equipment.weapon': {
-              name: 'Training Sword',
-              power: 0.75,
-              position: 'weapon',
-              attackType: 'melee',
-              previousOwners: []
-            },
-            inventory: {
-              equipment: [],
-              items: []
-            },
-            stats: {
-              str: 1,
-              dex: 1,
-              end: 1,
-              int: 1,
-              luk: 1
-            },
-            spells: [],
-            isOnline: true,
-            createdAt: new Date().getTime(),
-            events: 0,
-            gambles: 0,
-            stole: 0,
+    return new Promise((resolve, reject) => Player.update({},
+      {
+        $set: {
+          class: 'Wanderer',
+          health: 105,
+          mana: 50,
+          experience: {
+            current: 0,
+            total: 0
+          },
+          map: MapClass.getRandomTown(),
+          level: 1,
+          gold: {
+            current: 0,
             stolen: 0,
-            spellCasted: 0,
-            currentBounty: 0,
-            kills: {
-              mob: 0,
-              player: 0
-            },
-            battles: {
-              won: 0,
-              lost: 0,
-              firstDeath: 0
-            },
-            deaths: {
-              mob: 0,
-              player: 0,
-              firstDeath: 'never'
-            },
-            pastEvents: [],
-            pastPvpEvents: []
-          }
-        },
-        {
-          multi: true
-        }, (err, result) => {
-          if (err) {
-            disconnect();
-            return reject(err);
-          }
-          disconnect();
-          return resolve(result);
-        });
-    });
-  }
-
-  deleteAllPlayers() {
-    connect();
-    return new Promise((resolve, reject) => {
-      return Player.remove({}, (err, result) => {
+            stole: 0,
+            total: 0
+          },
+          'equipment.helmet': {
+            name: 'Nothing',
+            power: 0.15,
+            previousOwners: []
+          },
+          'equipment.armor': {
+            name: 'Linen Shirt',
+            power: 0.75,
+            position: 'armor',
+            previousOwners: []
+          },
+          'equipment.weapon': {
+            name: 'Training Sword',
+            power: 0.75,
+            position: 'weapon',
+            attackType: 'melee',
+            previousOwners: []
+          },
+          inventory: {
+            equipment: [],
+            items: []
+          },
+          stats: {
+            str: 1,
+            dex: 1,
+            end: 1,
+            int: 1,
+            luk: 1
+          },
+          spells: [],
+          isOnline: true,
+          createdAt: new Date().getTime(),
+          events: 0,
+          gambles: 0,
+          stole: 0,
+          stolen: 0,
+          spellCasted: 0,
+          currentBounty: 0,
+          kills: {
+            mob: 0,
+            player: 0
+          },
+          battles: {
+            won: 0,
+            lost: 0,
+            firstDeath: 0
+          },
+          deaths: {
+            mob: 0,
+            player: 0,
+            firstDeath: 'never'
+          },
+          pastEvents: [],
+          pastPvpEvents: []
+        }
+      },
+      {
+        multi: true
+      }, (err, result) => {
         if (err) {
           disconnect();
           return reject(err);
         }
         disconnect();
         return resolve(result);
-      });
-    });
+      }));
+  }
+
+  deleteAllPlayers() {
+    connect();
+    return new Promise((resolve, reject) => Player.remove({}, (err, result) => {
+      if (err) {
+        disconnect();
+        return reject(err);
+      }
+      disconnect();
+      return resolve(result);
+    }));
   }
 
 }
