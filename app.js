073da032c7c2b2b7c9d98d55a33b47306d262382
Sunny Huang <sizzlorox@gmail.com>
2018-05-21 14:16:56 -0300

    refactor(Helper): added random seed rng and removed jade engine

--- app.js
+++ app.js
@@ -11,7 +11,6 @@ console.log(`${process.env.NODE_ENV.includes('production') ? 'Running Production
 
 // Preperation for the website that allows others to let this bot join their discord!
 app.set('views', `${__dirname}/views`);
-app.set('view engine', 'jade');
 app.use('/', router);
 app.listen(PORT, () => console.log(`Idle RPG web listening on port ${PORT}!`));
 
--- idle-rpg/utils/Helper.js
+++ idle-rpg/utils/Helper.js
@@ -1,8 +1,11 @@
 const fs = require('fs');
+const seedrandom = require('seedrandom');
 const enumHelper = require('../utils/enumHelper');
 const { moveLog, actionLog, errorLog, infoLog } = require('../utils/logger');
 const { battleDebug, eventDebug, guildID } = require('../../settings');
 const messages = require('../game/data/messages');
+
+const RNG = seedrandom();
 
 class Helper {
   printBattleDebug(debugMsg) {
@@ -19,7 +22,7 @@ class Helper {
 
   randomBetween(min, max, decimal, exclude) {
     // https://stackoverflow.com/questions/15594332/unbiased-random-range-generator-in-javascript
-    if (arguments.length < 2) return (Math.random() >= 0.5);
+    if (arguments.length < 2) return (RNG() >= 0.5);
 
     let factor = 1;
     let result;
@@ -28,9 +31,7 @@ class Helper {
     }
 
     do {
-      //result = (Math.random() * (max - min)) + min;
-      // https://en.wikipedia.org/wiki/Power_law
-      result = Math.ceil(Math.exp(Math.random() * (Math.log(max) - Math.log(min))) * min);
+      result = (RNG() * (max - min)) + min;
       result = Math.round(result * factor) / factor;
     } while (result === exclude);
     return result;
