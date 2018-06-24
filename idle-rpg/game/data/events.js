2a0722f75ef17a4f979259ed8022dd3cb2ae7d35
Sunny Huang <sizzlorox@gmail.com>
2018-06-24 05:05:34 -0300

    fix(events): spacing in pm message

--- idle-rpg/game/data/events.js
+++ idle-rpg/game/data/events.js
@@ -98,8 +98,8 @@ ${mobListResult.join('\n')}`;
       ? `${mobFleeCountString} just fled from ${Helper.generatePlayerName(results.attacker, true)}!\n`
       : `${Helper.generatePlayerName(results.attacker, true)} just fled from ${mobFleeCountString}!\n`);
     eventLog = eventLog.concat(results.attackerDamage > results.defenderDamage
-      ? `${mobFleeCountString} fled from you! [${expGain}exp]\n`
-      : `You fled from ${mobFleeCountString}! [${expGain}exp]\n`);
+      ? `${mobFleeCountString} fled from you! [${expGain} exp]\n`
+      : `You fled from ${mobFleeCountString}! [${expGain} exp]\n`);
   }
 
   if (mobKillCountString) {
