009bc2948ca4b05d14b20c50c9397da68fc73c3b
Sunny Huang <sizzlorox@gmail.com>
2018-07-12 13:21:00 -0300

    refactor(V2): bot creating necessary channels on invited guilds

--- app.js
+++ app.js
@@ -2,17 +2,22 @@ require('dotenv').config();
 
 const express = require('express');
 const router = require('./web/routes/index');
+if (process.env.NODE_ENV.includes('production')) {
+  const discord = require('./idle-rpg/bots/discord');
+} else {
+  const DiscordBot = require('./idle-rpg/v2/DiscordBot');
+}
 
-const app = express();
+// const app = express();
 const { errorLog } = require('./idle-rpg/utils/logger');
 const { PORT } = process.env;
 
 console.log(`${process.env.NODE_ENV.includes('production') ? 'Running Production Env' : 'Running Development Env'}`);
 
 // Preperation for the website that allows others to let this bot join their discord!
-app.set('views', `${__dirname}/views`);
-app.use('/', router);
-app.listen(PORT, () => console.log(`Idle RPG web listening on port ${PORT}!`));
+// app.set('views', `${__dirname}/views`);
+// app.use('/', router);
+// app.listen(PORT, () => console.log(`Idle RPG web listening on port ${PORT}!`));
 
 process.on('unhandledRejection', (err) => {
   console.log(err);
--- web/routes/index.js
+++ web/routes/index.js
@@ -1,7 +1,6 @@
 const express = require('express');
 
 const router = express.Router();
-const discord = require('../../idle-rpg/bots/discord');
 
 router.use((req, res, next) => {
   console.log(Date.now());
