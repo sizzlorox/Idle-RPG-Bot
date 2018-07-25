2557705a73047c45040ca6c460aa479c6fea8a36
sizzlorox <sizzlorox@gmail.com>
2018-07-25 10:29:56 +0800

    update(DiscordBot): stream plugin to not check old user obj

--- idle-rpg/v2/DiscordBot.js
+++ idle-rpg/v2/DiscordBot.js
@@ -82,11 +82,11 @@ class DiscordBot {
 
 
     this.bot.on('guildMemberUpdate', (oldMember, newMember) => {
-      if (oldMember.guild.id !== guildID) {
+      if (newMember.guild.id !== guildID) {
         return;
       }
 
-      if (newMember.presence.game && newMember.presence.game.streaming && !oldMember.presence.game) {
+      if (newMember.presence.game && newMember.presence.game.streaming) {
         newMember.guild.channels.find(channel => channel.name === 'stream-plug-ins' && channel.type === 'text')
           .send(`${newMember.displayName} has started streaming \`${newMember.presence.game.name}\`! Go check the stream out if you're interested!\n<${newMember.presence.game.url}>`);
       }
