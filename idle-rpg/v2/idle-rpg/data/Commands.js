aadb89e5a5931a7a12cb3dd28fba0b44186fc50f
sizzlorr <sizzlorox@gmail.com>
2019-04-28 13:39:05 +0800

    chore(Commands): max changed from 0 to 1 in multiplier

--- idle-rpg/v2/idle-rpg/data/Commands.js
+++ idle-rpg/v2/idle-rpg/data/Commands.js
@@ -251,7 +251,7 @@ ${rankString}\`\`\``);
           actionsChannel.send(this.setImportantMessage(`${player.name}${player.titles.current !== 'None' ? ` the ${player.titles.current}` : ''} just cast${calcAmount > 1 ? ` ${calcAmount}x ` : ' '}${spell}!!\nCurrent Active Bless: ${guildConfig.spells.activeBless}\nCurrent Multiplier is: ${guildConfig.multiplier}x`));
           setTimeout(async () => {
             const newLoadedConfig = await this.Database.loadGame(player.guildId);
-            newLoadedConfig.multiplier = Math.max(0, newLoadedConfig.multiplier - calcAmount);
+            newLoadedConfig.multiplier = Math.max(1, newLoadedConfig.multiplier - calcAmount);
             newLoadedConfig.spells.activeBless = Math.max(0, newLoadedConfig.multiplier - calcAmount);
             newLoadedConfig.multiplier = newLoadedConfig.multiplier <= 0 ? 1 : newLoadedConfig.multiplier;
             await this.Database.updateGame(player.guildId, newLoadedConfig);
