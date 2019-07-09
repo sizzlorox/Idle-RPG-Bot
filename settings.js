893195ec6727bb1d31fff0749f1c3c0ae1dc63ff
Macley <26381427+Macley-Kun@users.noreply.github.com>
2019-07-09 11:09:06 +0200

    Update settings.js

--- settings.js
+++ settings.js
@@ -10,7 +10,7 @@ const settings = {
   minimalTimer: process.env.NODE_ENV.includes('production') ? process.env.MIN_TIMER : process.env.TEST_MIN_TIMER,
   maximumTimer: process.env.NODE_ENV.includes('production') ? process.env.MAX_TIMER : process.env.TEST_MAX_TIMER,
   botOperators: process.env.DISCORD_BOT_OPERATORS_ID.replace(' ', '').split(','),
-  mongoDBUri: process.env.MONGODB_URI ? process.env.JAWSDB_URL,
+  mongoDBUri: process.env.MONGODB_URI ? process.env.MONGODB_URI : process.env.JAWSDB_URL,
   starterTown: [3, 5],
   multiplier: 1
 };
